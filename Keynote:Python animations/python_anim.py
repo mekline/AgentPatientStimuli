1ff685225c96a2e0ee81480f76130c8f7138c1e5
lajordan <lilyjordan@dhcp-18-111-34-216.dyn.MIT.EDU>
2016-03-10 12:40:58 -0500

    image comments, timing

--- Keynote:Python animations/python_anim.py
+++ Keynote:Python animations/python_anim.py
@@ -1,12 +1,23 @@
-import pygame, sys
+import pygame, sys, time
 from pygame.locals import  *
 
 #set up pygame
 pygame.init()
 
 #set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation') #Caption on top of window
+
+#set up direction variables
+RIGHT = 1
+LEFT = 2
+UP = 3
+DOWN = 4
+
+#Establish movement speed
+MOVESPEED = 4
 
 #set up the colors
 BLACK = (0, 0, 0)
@@ -19,12 +30,12 @@ BLUE = (0, 0, 255)
 #draw the black background onto the windowSurface
 windowSurface.fill(BLACK)
 
-#load image
+#load image of agent
 eyeShape = pygame.image.load('3eyecopy.png').convert()
 
 windowSurface.blit(eyeShape,(30,130))
 
-# draw a blue circle onto the surface
+# draw a blue circle onto the surface (this is the patient)
 pygame.draw.circle(windowSurface, BLUE, (300, 180), 50, 0)
 
 
@@ -42,3 +53,4 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
