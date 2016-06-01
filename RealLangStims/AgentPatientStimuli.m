0b0842d52736865ca2e7d9aa8d794b6bfca844d4
lajordan <lilyjordan@dhcp-18-111-18-199.dyn.mit.edu>
2016-06-01 10:51:42 -0400

    Changed name to PastSentences such that Psychtoolbox doesn't crash

--- RealLangStims/AgentPatientStimuli.m
+++ RealLangStims/AgentPatientStimuli.m
@@ -128,7 +128,7 @@ function AgentPatientStimuli(subjID, list, order, run)
     %from the materials file and save them to a .mat file
     if run==1
         %Read in all materials from a csv
-        materials_filename = 'info_A1.csv';
+        materials_filename = 'AgentPatientStimuli_materials.csv';
         all_materials = readtable(materials_filename);
     
         %Extract only the materials for the given list
