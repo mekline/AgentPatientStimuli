58319b061c7463e5b7419bf1752b2a14780b1697
lajordan <lilyjordan@dhcp-18-111-124-149.dyn.mit.edu>
2016-06-01 17:51:03 -0400

    Created TextOnImage script that turns text into a jpeg

---
+++ RealLangStims/TextOnImage.m
@@ -0,0 +1,14 @@
+function TextOnImage(text)
+    IMAGE_DIR = fullfile(pwd, 'images');
+    fileToSave = ['AgentPatientStimuli_image.jpg'];
+    fileToSave = fullfile(IMAGE_DIR, fileToSave);
+    
+    I = imread('blank-white-rectangle.png');
+    position = [1000 1000];
+    RGB = insertText(I,position,text,'FontSize',200,'BoxOpacity',0);
+    
+    figure
+    imshow(RGB)
+    
+    imwrite(RGB,fileToSave,'jpg')
+end
