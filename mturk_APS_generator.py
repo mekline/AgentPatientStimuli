9e1c53f0d0ed29e0fde3df51c1d179152efee442
lajordan <lajordan@mit.edu>
2016-07-06 12:32:00 -0400

    Wrote script, created mturk_images_A.csv

--- mturk_APS_generator.py
+++ mturk_APS_generator.py
@@ -1,4 +1,4 @@
 for i in range(1,121):
     text_file = open("APS_mturk.txt", "a")
-    image_text = '\n<fieldset>\n<div><img alt="${IMAGE_%s}" class="imagecontainer" src="${image_url}" /></div>\n<div class="inputfields">\n<div class="form-group"><label>Description:</label> <input class="form-control" name="INPUT_%d" required="" size="30" type="text" /></div>\n</div>\n</fieldset>\n'
-    text_file.write(image_text % (i,i))
+    image_text = '\n<fieldset>\n<div><img alt="${IMAGE_%s}" class="imagecontainer" src="${IMAGE_%s}" /></div>\n<div class="inputfields">\n<div class="form-group"><label>Description:</label> <input class="form-control" name="INPUT_%d" required="" size="30" type="text" /></div>\n</div>\n</fieldset>\n'
+    text_file.write(image_text % (i,i,i))
