c3e9044d5cc54625c0be2278653bdcc73fa650e7
lajordan <lajordan@mit.edu>
2016-07-06 23:19:02 -0400

    Redid APS_mturk.txt based on new generator

--- mturk_APS_generator.py
+++ mturk_APS_generator.py
@@ -1,6 +1,4 @@
-text_file = open("APS_mturk.txt", "a")
-
 for i in range(1,121):
-    text_file = open("APS_mturk_html.rtf", "a")
-    image_text = "\n<fieldset>\n<div><img alt="image_url" class="imagecontainer" src="${image_url}" /></div>\n<div class="inputfields">\n<div class="form-group"><label>Description:</label> <input class="form-control" name="INPUT_1" required="" size="30" type="text" /></div>\n</div>\n</fieldset>"
+    text_file = open("APS_mturk.txt", "a")
+    image_text = '\n<fieldset>\n<div><img alt="${IMAGE_%s}" class="imagecontainer" src="${image_url}" /></div>\n<div class="inputfields">\n<div class="form-group"><label>Description:</label> <input class="form-control" name="INPUT_%d" required="" size="30" type="text" /></div>\n</div>\n</fieldset>\n'
     text_file.write(image_text % (i,i))
